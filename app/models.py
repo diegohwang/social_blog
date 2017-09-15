18030a781daac5f0c4240ec63d8bd1e134dfd5ec
unknown <huangjinming1@360buyad.local>
2017-09-15 17:49:55 +0800

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.INTEGER, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.INTEGER, db.ForeignKey('roles.id'))
+    #加入密码散列
+    password_hash = db.Column(db.String(128))
+    #增加确认用户账户字段
+    confirmed = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    #生成校验令牌token
+    def generate_confirmation_token(self, expriration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expriration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+from . import login_manager
+#加载用户的回调函数
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
---
+++ configurations/config.py
@@ -0,0 +1,45 @@
+import ConfigParser
+import os
+import sys
+sys.path.append('.')
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+conf = ConfigParser.ConfigParser()
+conf.read('configurations/etc.conf')
+
+
+class Config:
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <diegohwang@163.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    '''
+
+
+    SECRET_KEY = conf.get('app', 'SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = conf.getboolean('app', 'SQLALCHEMY_COMMIT_ON_TEARDOWN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = conf.getboolean('app', 'SQLALCHEMY_TRACK_MODIFICATIONS')
+    FLASKY_MAIL_SUBJECT_PREFIX = conf.get('mail', 'FLASKY_MAIL_SUBJECT_PREFIX')
+    FLASKY_MAIL_SENDER = conf.get('mail', 'FLASKY_MAIL_SENDER')
+    FLASKY_ADMIN = conf.get('mail', 'FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = conf.get('mail', 'MAIL_SERVER')
+    MAIL_PORT = conf.getint('mail', 'MAIL_PORT')
+    MAIL_USE_TLS = conf.getboolean('mail', 'MAIL_USE_TLS')
+    MAIL_USERNAME = conf.get('mail', 'MAIL_USERNAME')
+    MAIL_PASSWORD = conf.get('mail', 'MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = conf.get('app', 'SQLALCHEMY_DATABASE_URI')
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
